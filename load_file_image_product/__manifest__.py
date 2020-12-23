f034fe1f19053fcf693c214d3066bd55a559cc70
Katia Logas <klogas@aitic.com.ar>
2020-12-23 17:39:58 -0300

    Importar imagenes de producto

---
+++ load_file_image_product/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Katia Logas Fonseca
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Import file with product images',
+    'version': '0.0.1',
+    'author': 'Katia Logas Fonseca',
+    'license': '',
+    'category': '',
+    'website': '',
+    'depends': ['base','web', 'product'],
+    'data': [
+             'views/load_file_image_product_view.xml',
+             'views/large_file_record_view.xml',
+             'views/load_file_image_product_menu.xml'],
+    'qweb': [
+             'static/src/xml/large_file_load.xml'],
+    'installable': True,
+    'auto_install': True,
+}
---
+++ load_file_image_product/models/res_company.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Katia Logas Fonseca
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+import csv
+import os
+
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+from dateutil.relativedelta import relativedelta
+import logging
+import requests
+import os, sys, tempfile, traceback
+from hashlib import md5
+from pysimplesoap.simplexml import SimpleXMLElement
+_logger = logging.getLogger(__name__)
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    def importImage(self, save_path):
+        PRODUCT = self.env['product.template']
+        with open(save_path) as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=',')
+            count = 0
+            for row in readCSV:
+                if readCSV.line_num != 1:
+                    print('Importing: ', row[1])
+                    try:
+                        external_ids = self.env['ir.model.data'].sudo().search([('model', '=', 'product.template')], order='id')
+                        if external_ids:
+                            external_id = external_ids.filtered(lambda x: x.complete_name == row[1])
+                            if external_id:
+                                PRODUCT.search([('id', '=', external_id.res_id)]).write({
+                                    'image_1920': row[2]
+                                })
+                                if count % 2 == 0:
+                                    PRODUCT._cr.commit()
+                        # PRODUCT.search([]).get_external_id().items()
+                        # PRODUCT.search([])._get_external_ids().items()
+                    except:
+                        pass
+
+        raise Warning('Import Done!')
+
+
+
